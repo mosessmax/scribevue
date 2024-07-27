9fcf71ffb730317efe0a8648aed029f052f99680
adebayo-moses <adebayomoses597@gmail.com>
2024-07-27 15:35:07 +0100

    build: homeview all done.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <!-- <HelloWorld /> -->
 
   <RouterView />
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,21 @@
 </script>
 
 <template>
-  <main>
-    <h1>Home</h1>
-    <p>Welcome to the Home page</p>
-  </main>
+ <div class="wrapper">
+      <img src="/src/assets/welcome-home.gif" height="200" width="" />
+      <br />
+      <div class="title">
+        welcome home, manchi
+      </div>
+      <div class="content">
+        Finally, a writing app hacked just for you! Time to crank out some epic content.<br>
+        Just adding a random second line to keep things looking legit.
+      </div>
+      <button id="home__button">
+        <router-link class="orange" to="/select"> let’s get writing.</router-link>
+      </button>
+      <div class="home__footer">
+        built by mosessmax. on vuejs3 + typescript
+      </div>
+    </div>
 </template>
